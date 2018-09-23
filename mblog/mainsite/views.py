0e3ab9e6eaaf178f527be6e24b68b163ddc7f2ef
gotSomeCool <wsljw222@gmail.com>
2018-09-23 15:15:40 +0800

    server

---
+++ mblog/mainsite/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+from .models import Post
+from django.template.loader import get_template
+from datetime import datetime
+from markdown2 import markdown
+# Create your views here.
+
+def homepage(request):
+    template=get_template('index.html')
+    posts=Post.objects.all()
+
+    now=datetime.now()
+    html=template.render(locals())
+    return HttpResponse(html)
+
+def showpost(request,slug):
+    template=get_template('post.html')
+    try:
+        post=Post.objects.get(slug=slug)
+        if post is not None:
+            post.body=markdown(post.body)
+            html = template.render(locals())
+            return HttpResponse(html)
+    except:
+        return redirect('/')
